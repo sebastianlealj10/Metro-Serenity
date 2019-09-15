5c3f9732b025d7431911729f73e38c1429dc8f78
Sebastian Leal <sebastianleaj10@gmail.com>
2019-09-14 20:49:12 -0500

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+package steps;
+
+public class Calculator {
+    private int total;
+    public Calculator(int amount) {
+        this.total = amount;
+    }
+
+    public void add(int amount) {
+        total = total + amount;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
